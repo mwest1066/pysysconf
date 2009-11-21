b84796312bc70a4b2831756a1f58bfea1860d195
root <root@hamilton.mechse.illinois.edu>
2009-11-20 21:20:05 -0600

    added 0.3.4 build

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name = "pysysconf",
-      version = "0.3.3",
+      version = "0.3.4",
       description = "Python System Configuration library",
       author = "Matthew West",
       author_email = "mwest@illinois.edu",
