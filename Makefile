cc9709c43cfcae1835c1e120665299c266d047af
Matthew West <mwest@mace.localdomain>
2010-11-05 11:11:42 -0500

    added 0.3.6 build; updated build scripts

--- Makefile
+++ Makefile
@@ -1,16 +1,17 @@
 info:
-	@echo "make dist:     Build .tar.gz. and .rpm distribution archives (remember to"
+	@echo "make sdist:    Build .tar.gz. and .rpm distribution archives (remember to"
 	@echo "               bump the version number in setup.py and ChangeLog first)"
+	@echo "make rpm_fc14: Make an RPM on Fedora 14"
 	@echo "make install:  Install on current machine"
 
 dist: sdist bdist
 
-sdist: pysysconf.html README MANIFEST Makefile test_pysysconf.py \
+sdist: pysysconf.html README MANIFEST.in Makefile test_pysysconf.py \
 	setup.py pysysconf.html
 	python setup.py sdist
 
-bdist: pysysconf.py
-	python setup.py bdist --format=rpm
+rpm_fc14: pysysconf.py
+	python setup.py bdist_rpm --release="1.fc14"
 
 pysysconf.html: pysysconf.py
 	pydoc -w pysysconf
--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from distutils.core import setup
 setup(name = "pysysconf",
-      version = "0.3.5",
+      version = "0.3.6",
       description = "Python System Configuration library",
       author = "Matthew West",
       author_email = "mwest@illinois.edu",
       url = "http://lagrange.mechse.illinois.edu/mwest/pysysconf/",
       py_modules = ["pysysconf"],
+      license = "GPLv2+",
+      platforms = ["Linux"],
+      long_description = "A collection of functions to enable simple system configurations scripts. Primarily targeted at Red Hat and Fedora Linux.",
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Systems Administration',
+        ],
       )
